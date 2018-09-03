3850ae78d29c170cdba4c45d95ac193ac1189494
Diego Marczal <dmarczal@gmail.com>
2018-09-03 17:07:53 -0300

    Added image to admin using active storage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,6 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 end
