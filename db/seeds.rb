219075ace5381f36dcd393b9f196e68113addbcf
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-09-09 17:31:55 -0300

    adicionado seeds de categoria e titulacao

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
 Admin.create_with(name: 'Administrador', password: '123456').find_or_create_by!(email: 'admin@admin.com')
 
+ProfessorCategory.create_with(name: 'Efetivo')
+ProfessorCategory.create_with(name: 'Temporario')
+
+ProfessorTitle.create_with(description: 'Especialista', abbrev: 'Esp.')
+ProfessorTitle.create_with(description: 'Mestre', abbrev: 'Me.')
+ProfessorTitle.create_with(description: 'Doutor', abbrev: 'Dr.')
+
 100.times do |n|
   Professor.create(name: "Name#{n}", lattes: "lates#{n}",
                    occupation_area: "area de ocupacao#{n}", email: "email#{n}@email.com")
