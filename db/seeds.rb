49807c5715d5dc9aa6c257f0223faa8364a59dab
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-09-09 19:22:13 -0300

    Added CategoryRecommendation database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
 Admin.create_with(name: 'Administrador', password: '123456').find_or_create_by!(email: 'admin@admin.com')
+
+categories = [
+  {name: 'Documentário'},
+  {name: 'Filme'},
+  {name: 'Livro'},
+  {name: 'Seriado'},
+]
+
+categories.each do |category|
+  CategoryRecommendation.create_with(name: category[:name]).find_or_create_by!(name: category[:name])
+end
