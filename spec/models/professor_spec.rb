eed9ecefb51d8908abfa479e5423c5044ad6e2cb
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-09-10 20:05:29 -0300

    partial refactor, tests model implementeds

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:lattes) }
+    it { is_expected.to validate_presence_of(:occupation_area) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:professor_title) }
+    it { is_expected.to validate_presence_of(:professor_category) }
+  end
 end
--- spec/models/professor_title_spec.rb
+++ spec/models/professor_title_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ProfessorTitle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it { is_expected.to validate_presence_of(:description) }
+    it { is_expected.to validate_presence_of(:abbrev) }
+  end
 end
