2297164a8da41d383d28293a0ff3f1a09cfac70d
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-09-10 23:13:42 -0300

    Added category_recommendation association validation on recommendation model test

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Recommendation, type: :model do
   describe 'validates' do
     it { is_expected.to validate_presence_of(:title) }
     it { is_expected.to validate_presence_of(:description) }
+    it { should belong_to(:category_recommendation) }
   end
 end
