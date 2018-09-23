4c098debce83d19ede269b4a206b0443ce13858b
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-09-23 13:19:48 -0300

    refatoracao do controller e i18n

--- app/controllers/admins/professors_controller.rb
+++ app/controllers/admins/professors_controller.rb
@@ -2,7 +2,7 @@ class Admins::ProfessorsController < Admins::BaseController
   before_action :mount_professor, only: [:edit, :new, :show]
 
   def index
-    @professors = Professor.all
+    @professors = Professor.order(name: :asc)
   end
 
   def new
@@ -12,32 +12,36 @@ class Admins::ProfessorsController < Admins::BaseController
   def create
     @professor = Professor.new(professor_params)
     if @professor.save
-      flash[:success] = 'Professor criado com sucesso'
+      flash[:success] = I18n.t('flash.actions.create.m', resource_name: Professor.model_name.human)
       redirect_to admins_professors_path
     else
-      flash[:error] = I18n.t('simple_form.error_notification.default_message')
+      flash.now[:error] = I18n.t('flash.actions.errors')
       render :new
     end
   end
 
-  def edit; end
+  def edit;
+  end
 
   def update
     @professor = find_user
     if @professor.update_attributes(professor_params)
+      flash[:success] = I18n.t('flash.actions.update.m', resource_name: Professor.model_name.human)
       redirect_to admins_professor_path(@professor)
     else
+      flash.now[:error] = I18n.t('flash.actions.errors')
       render :edit
     end
   end
 
   def destroy
     find_user.destroy
-    flash[:success] = 'Professor removido com sucesso!'
+    flash[:success] = I18n.t('flash.actions.destroy.m', resource_name: Professor.model_name.human)
     redirect_to admins_professors_path
   end
 
-  def show; end
+  def show;
+  end
 
   private
 
@@ -50,9 +54,12 @@ class Admins::ProfessorsController < Admins::BaseController
   end
 
   def professor_params
-    params.require(:professor).permit(:name, :lattes,
+    params.require(:professor).permit(:name,
+                                      :lattes,
                                       :occupation_area,
-                                      :email, :professor_title_id, :professor_category_id)
+                                      :email,
+                                      :professor_title_id,
+                                      :professor_category_id)
   end
 
   def find_user
