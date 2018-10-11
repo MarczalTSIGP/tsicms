1a39eadd85ab06dabbea4bca73030ca08b06f7cb
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-10-11 16:16:21 -0300

    mudanças gereno professor

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,12 +1,11 @@
 class Professor < ApplicationRecord
   validates :name, presence: true
-  validates :gender, presence: true
   validates :lattes, presence: true, format: { with: URI.regexp }
   validates :occupation_area, presence: true
   validates :email, presence: true, format: { with: Devise.email_regexp }
   validates :professor_title, presence: true
   validates :professor_category, presence: true
-
+  enum genders: { male: 'Masculino', female: 'Feminino'}
   belongs_to :professor_title
   belongs_to :professor_category
   mount_uploader :image, ProfessorImageUploader
--- spec/factories/professors.rb
+++ spec/factories/professors.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :professor do
     sequence(:name) {|n| "Name#{n}"}
-    choose("Male")
+    choose(:gender)
     sequence(:lattes) {|n| "http://lattes.com.#{n}"}
     sequence(:occupation_area) {|n| "Artificial Inteligence #{n}"}
     sequence(:email) {|n| "email#{n}@email.com"}
     professor_title
     professor_category
-    
     image { FileSpecHelper.image }
   end
 end
