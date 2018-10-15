7deb0f8311d3738b030b3d069eb1cbd76452a0e0
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-10-15 20:12:04 -0300

    Added static page validations

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -1,2 +1,13 @@
 class StaticPage < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :permalink, presence: true,
+                        uniqueness: { case_sensitive: false },
+                        format: { with: /\A[a-zA-Z]+\z/, message: 'Only allows letters, numbers and dashes' }
+
+  # before_create :create_slug
+  #
+  # def create_slug
+  #   self.permalink = self.permalink.parameterize
+  # end
 end
