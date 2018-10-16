b6c15968e0f3481a81b6275886b071a945fd8341
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-10-16 12:38:08 -0300

    Adjust StaticPage permalink validation

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -3,11 +3,5 @@ class StaticPage < ApplicationRecord
   validates :content, presence: true
   validates :permalink, presence: true,
                         uniqueness: { case_sensitive: false },
-                        format: { with: /\A[a-zA-Z]+\z/, message: 'Only allows letters, numbers and dashes' }
-
-  # before_create :create_slug
-  #
-  # def create_slug
-  #   self.permalink = self.permalink.parameterize
-  # end
+                        format: { with: /\A[\w\.\-]+\z/, message: I18n.t('validation.permalink') }
 end
