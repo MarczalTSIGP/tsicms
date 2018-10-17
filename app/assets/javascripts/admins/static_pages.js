152e3532bee99faaecef9b50eb5311318273d2fd
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-10-16 21:00:12 -0300

    Added script to generate permalink for the first time

--- app/assets/javascripts/admins/static_pages.js
+++ app/assets/javascripts/admins/static_pages.js
@@ -2,4 +2,22 @@
 
 $(document).on('turbolinks:load', function() {
   var simplemde = new SimpleMDE({ element: document.getElementById('static_page_content') });
+
+  $('#static_page_title').on('blur', function () {
+    var content = $(this).val();
+
+    var permalinkElement = $('#static_page_permalink');
+    var permalink = permalinkElement.val();
+
+    if (permalink === "") {
+      generateSlug(permalinkElement, content);
+    }
+  })
 });
+
+function generateSlug(permalinkElement, content) {
+  content = content.replace(/\s/gi, '-');
+  var slug = content.replace(/[^\w\.\-]/gi, '');
+
+  permalinkElement.val(slug.toLowerCase());
+}
