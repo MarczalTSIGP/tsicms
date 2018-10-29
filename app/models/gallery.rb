a9b96fcfe467ac282985e70f9b3d95a2104627ea
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-10-28 22:30:01 -0300

    Change Gallery.context enum definition to case-sensitive

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ApplicationRecord
-  validates :context, presence: true, uniqueness: { case_sensitive: false }
   enum context: { course: 'course', static_page: 'static_page' }
+  validates :context, presence: true, uniqueness: { case_sensitive: true }
   has_many :pictures, dependent: :destroy
 end
