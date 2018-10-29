8f0b4073bc0b7d271a8803f5cd1df1eac354a7a6
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-10-28 22:17:19 -0300

    crud de vaga de estagio concluido

--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -1,9 +1,9 @@
 class Trainee < ApplicationRecord
   belongs_to :company
-  belongs_to :trainee_statuses
+  belongs_to :trainee_status
 
   validates :company, presence: true
-  validates :trainee_statuses, presence: true
+  validates :trainee_status, presence: true
   validates :title, presence: true
   validates :description, presence: true
 end
--- db/migrate/20181027024341_create_trainee_statuses.rb
+++ db/migrate/20181027024341_create_trainee_statuses.rb
@@ -1,7 +1,7 @@
 class CreateTraineeStatuses < ActiveRecord::Migration[5.2]
   def change
     create_table :trainee_statuses do |t|
-      t.string :description
+      t.string :name
 
       t.timestamps
     end
