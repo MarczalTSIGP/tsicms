e450f5889e03bfd4eee159217d3f4c2301a320aa
Lucas Antonio Ramos Sartori <faltou.criatividade0@gmail.com>
2018-11-04 19:32:08 -0200

    i18n e header da pagina publica de estagios

--- app/controllers/trainees_controller.rb
+++ app/controllers/trainees_controller.rb
@@ -1,6 +1,7 @@
 class TraineesController < ApplicationController
   add_breadcrumb I18n.t('breadcrumbs.trainees.name'), :trainees_path
   def index
+    @static_page = StaticPage.find_by!(title: I18n.t('helpers.trainee'))
     @trainees = Trainee.order(title: :asc)
   end
 
