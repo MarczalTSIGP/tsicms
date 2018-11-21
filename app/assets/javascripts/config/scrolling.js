53462259568df96d0d84be2fa1f0e75c507ed2be
Kuchla <josni_kuchla@hotmail.com>
2018-11-21 20:21:09 +0000

    Fix Cognitive Complexity - Scrolling js decreased number of lines

--- app/assets/javascripts/config/scrolling.js
+++ app/assets/javascripts/config/scrolling.js
@@ -38,34 +38,11 @@ TSICMS.scrolling.reveals = function () {
   // Scroll reveal calls
   window.sr = ScrollReveal();
 
-  sr.reveal('.sr-icon-1', {
-    delay: 200,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-2', {
-    delay: 400,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-3', {
-    delay: 600,
-    scale: 0
-  });
-  sr.reveal('.sr-icon-4', {
-    delay: 800,
-    scale: 0
-  });
-  sr.reveal('.sr-button', {
-    delay: 200,
-    distance: '15px',
-    origin: 'bottom',
-    scale: 0.8
-  });
-  sr.reveal('.sr-contact-1', {
-    delay: 200,
-    scale: 0
-  });
-  sr.reveal('.sr-contact-2', {
-    delay: 400,
-    scale: 0
-  });
+  sr.reveal('.sr-icon-1', { delay: 200, scale: 0 });
+  sr.reveal('.sr-icon-2', { delay: 400, scale: 0 });
+  sr.reveal('.sr-icon-3', { delay: 600, scale: 0 });
+  sr.reveal('.sr-icon-4', { delay: 800, scale: 0 });
+  sr.reveal('.sr-button', { delay: 200, distance: '15px', origin: 'bottom', scale: 0.8 });
+  sr.reveal('.sr-contact-1', { delay: 200,scale: 0 });
+  sr.reveal('.sr-contact-2', { delay: 400, scale: 0 });
 }
