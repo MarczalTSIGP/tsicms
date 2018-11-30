ed0741dd6e2d351d7cbe34d9724a7bccf37f4a6d
Valdinei Moraes <valdineimoraes@alunos.utfpr.edu.br>
2018-11-30 16:50:26 -0200

    Spec ProfessorPeriod - falta revisão nos testes

--- spec/factories/professor_periods.rb
+++ spec/factories/professor_periods.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :period_professor do
-    date_entry { "2018-10-24" }
+  factory :professor_period do
+    date_entry { "2018-01-24" }
     date_out { "2018-10-24" }
     professor { nil }
   end
