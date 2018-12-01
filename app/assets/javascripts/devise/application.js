8cc759d03258900840dbab79ab883e0cb84125cb
Diego Marczal <dmarczal@gmail.com>
2018-12-01 10:40:00 -0200

    Fixed login page layout. Ignoring rubocop 'context' offenses of galleries

---
+++ app/assets/javascripts/devise/application.js
@@ -0,0 +1,26 @@
+// Rails
+//--------------------
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+
+
+// Gems
+//--------------------
+//= require jquery3
+//= require popper
+//= require bootstrap
+
+
+// Vendors
+//--------------------
+
+
+// Config files
+//--------------------
+//= require config/namespace
+//= require config/jquery.mask
+
+
+// App files
+//--------------------
