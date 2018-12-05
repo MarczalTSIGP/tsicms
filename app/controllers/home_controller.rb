d4a0155ab943947fecd478a268e71aed9c33ba30
Kuchla <josni_kuchla@hotmail.com>
2018-12-05 23:36:29 +0000

    Created public page to trainees and companies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   def index
     @discipline_monitors = DisciplineMonitor.order(year: :desc, semester: :desc)
     @faqs = Faq.order(created_at: :desc)
+    @trainees = Trainee.order(created_at: :desc)
+    @companies= Company.order(created_at: :desc)
   end
 end
