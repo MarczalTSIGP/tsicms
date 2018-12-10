0a61f561c6dc62921d5f667a7bb732df1086054d
Jonadabe Souza <jhonndabi.s.n@hotmail.com>
2018-12-10 20:07:53 -0200

    Splitted gallery list into two views, one to pictures, and another to documents

--- app/controllers/admins/galleries_controller.rb
+++ app/controllers/admins/galleries_controller.rb
@@ -1,5 +1,15 @@
 class Admins::GalleriesController < Admins::BaseController
   def index
     @gallery = Gallery.find_by!(context: params[:context])
+
+    if @gallery.contains_pictures
+      render :pictures
+    else
+      render :documents
+    end
   end
+
+  def pictures; end
+
+  def documents; end
 end
